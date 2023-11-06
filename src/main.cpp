4e18e9b95e4dd34daf020e2efc39c73bde63837a
Anatolii Cherednichenko <stdanatolii@gmail.com>
2023-11-06 18:21:14 +0000

    update

--- src/main.cpp
+++ src/main.cpp
@@ -6,63 +6,82 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main(int argc, char* argv[]) {
     try {
-        // Check if there are enough arguments
-        if (argc < 3) {
-            std::cerr << "Usage: " << argv[0] << " <directory> <filename> [--generate-data]" << std::endl;
-            return EXIT_FAILURE;
+        std::string outputDir = "data"; // Default output directory
+        std::string resultsFilename = "simulation_results.csv"; // Default results filename
+        std::string testDataFilename = "test_data.csv"; // Default filename for test data
+        std::string statsFilename = "statistics.csv"; // Default statistics filename
+        bool generateData = false;
+
+        // Default Simulation parameters
+        size_t numPaths = 10000;
+        long double initialPrice = 100.0;
+        long double volatility = 0.2;
+        long double drift = 0.05;
+        int steps = 252;
+
+        // Parse command line arguments
+        for (int i = 1; i < argc; ++i) {
+            std::string arg = argv[i];
+            if (arg == "--generate-data") {
+                generateData = true;
+            } else if (arg == "--outputDir" && i + 1 < argc) {
+                outputDir = argv[++i];
+            } else if (arg == "--numPaths" && i + 1 < argc) {
+                numPaths = std::stoull(argv[++i]);
+            } else if (arg == "--initialPrice" && i + 1 < argc) {
+                initialPrice = std::stold(argv[++i]);
+            } else if (arg == "--volatility" && i + 1 < argc) {
+                volatility = std::stold(argv[++i]);
+            } else if (arg == "--drift" && i + 1 < argc) {
+                drift = std::stold(argv[++i]);
+            } else if (arg == "--steps" && i + 1 < argc) {
+                steps = std::stoi(argv[++i]);
+            } else if (arg == "--resultsFile" && i + 1 < argc) {
+                resultsFilename = argv[++i];
+            } else if (arg == "--statsFile" && i + 1 < argc) {
+                statsFilename = argv[++i];
+            }
         }
 
-        // Parse arguments
-        std::string directory = argv[1];
-        std::string filename = argv[2];
-        bool generateData = (argc > 3 && std::string(argv[3]) == "--generate-data");
+        // Ensure the output directory exists
+        if (!fs::exists(outputDir)) {
+            fs::create_directories(outputDir);
+        }
 
-        // Simulation parameters
-        size_t numPaths = 10000;  // Adjust as necessary
-        long double initialPrice = 100.0; // Initial asset price
-        long double volatility = 0.2;     // Volatility
-        long double drift = 0.05;         // Drift (average return)
-        int steps = 252;             // Number of steps (e.g., trading days in a year)
-
-        // Create an instance of OutputSaver with the directory from arguments
-        IO::OutputSaver outputSaver(directory);
+        // Create instances of OutputSaver for saving results and test data
+        IO::OutputSaver outputSaver(outputDir);
 
         // Generate test data if the flag is present
         if (generateData) {
-            outputSaver.generateTestData(filename, numPaths, initialPrice, volatility, drift, steps);
+            std::cout << "Generating test data...\n";
+            outputSaver.generateTestData(testDataFilename, numPaths, initialPrice, volatility, drift, steps);
+            std::cout << "Test data generated and saved to " << testDataFilename << std::endl;
         }
 
-        // Run Monte Carlo simulation
+        // Run the Monte Carlo simulation
         std::cout << "Running Monte Carlo simulation...\n";
-        auto simulationResults = Simulation::StochasticModels::runMonteCarloSimulation(
+        std::vector<long double> simulationResults = Simulation::StochasticModels::runMonteCarloSimulation(
             initialPrice, volatility, drift, steps, numPaths);
 
+        // Convert simulation results to string for saving
+        std::vector<std::vector<std::string>> simulationResultsAsString = outputSaver.convertToCSVFormat(simulationResults);
+
+        // Save the simulation results
+        outputSaver.saveData(simulationResultsAsString, resultsFilename);
+        std::cout << "Simulation results saved to " << resultsFilename << std::endl;
+
         // Calculate statistics from the simulation results
-        std::cout << "Calculating statistics...\n";
-       // auto stats = Statistics::calculateStatistics(simulationResults);
+        // auto stats = Statistics::calculateStatistics(simulationResults);
+        // outputSaver.saveData(stats, statsFilename);
 
-        // Visualize the results (this is a placeholder for your actual visualization logic)
-        std::cout << "Generating visualizations...\n";
-      //  Visualization::generateCharts(simulationResults, "simulation_results.png");
-
-        // Save simulation results to a file
-        std::cout << "Saving simulation results...\n";
-        std::vector<std::vector<std::string>> simulationResultsAsString;
-        for (const auto& value : simulationResults) {
-            std::vector<std::string> row;
-            row.push_back(std::to_string(value));
-            simulationResultsAsString.push_back(row);
-        }
-        outputSaver.saveData(simulationResultsAsString, "simulation_results.csv");
-
-      //  outputSaver.saveData(simulationResults,"simulation_results.csv");
-
-        // Save statistics to a file
-        std::cout << "Saving statistics...\n";
-       // outputSaver.saveData("statistics.csv", stats);
+        // Visualize the results
+        // Visualization::generateCharts(simulationResults, outputDir + "/visualization.png");
 
         std::cout << "Monte Carlo simulation completed successfully.\n";
     } catch (const std::exception& e) {
